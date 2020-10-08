8ed7de9fdee4ad9191c8f72c45da9569503202d7
Yannick Gloster <yannickgloster@gmail.com>
2020-10-08 10:21:23 +0100

    Added Update Notifcation From Releases
    Every day, the bot will check for updates and if there is an update, it will send the bot owner a message to update the bot

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,14 +1,16 @@
+import aiohttp
 import discord
 import checks
 import traceback
 
 from bot import Discord_10man
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 class Utils(commands.Cog):
     def __init__(self, bot: Discord_10man):
         self.bot: Discord_10man = bot
+        self.check_update.start()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -53,6 +55,21 @@ class Utils(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Please specify an amount of messages to delete')
         traceback.print_exc()
+
+    @tasks.loop(hours=24)
+    async def check_update(self):
+        session = aiohttp.ClientSession()
+        async with session.get('https://api.github.com/repos/yannickgloster/discord-10man/releases/latest') as resp:
+            json = await resp.json()
+            if self.bot.version < json['tag_name'][1:]:
+                embed = discord.Embed(title=f'Discord 10man Update {json["tag_name"]}', url=json["html_url"])
+                embed.set_thumbnail(
+                    url="https://repository-images.githubusercontent.com/286741783/1df5e700-e141-11ea-9fbc-338769809f24")
+                embed.add_field(name='Release Notes', value=f'{json["body"]}', inline=False)
+                embed.add_field(name='Download', value=f'{json["html_url"]}', inline=False)
+                owner: discord.Member = (await self.bot.application_info()).owner
+                await owner.send(embed=embed)
+        await session.close()
 
     @commands.command(aliases=['version'], help='This command gets the bot information and version')
     async def about(self, ctx: commands.Context):
