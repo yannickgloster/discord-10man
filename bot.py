e5f89bf8a681822d3dbb1a9f77c61b0fb02964e6
Yannick Gloster <yannickgloster@gmail.com>
2020-10-19 22:43:52 +0100

    Fixed the enabling and disabling of the queue

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from typing import List
 from utils.server import WebServer
 from utils.csgo_server import CSGOServer
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 __dev__ = 745000319942918303
 
 class Discord_10man(commands.Bot):
