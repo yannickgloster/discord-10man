a453e07d6be0406fb1020be5cbf0b0c10dd347f9
Yannick Gloster <yannickgloster@gmail.com>
2022-02-05 12:30:00 +0000

    Added base files

---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
+
+const commands = [];
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+
+rest
+  .put(
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID,
+      process.env.GUILD_ID
+    ),
+    { body: commands }
+  )
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
